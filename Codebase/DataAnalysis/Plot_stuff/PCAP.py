2332d5882c824da86766b6a89f4a05f8fc899974
William Oskar Holthe Hirst <wohirst@hepp03.hpc.uio.no>
2022-11-12 13:05:37 +0100

    Created distribution plot for two best and worst PCA feats.

--- Codebase/DataAnalysis/Plot_stuff/PCAP.py
+++ Codebase/DataAnalysis/Plot_stuff/PCAP.py
@@ -3,7 +3,9 @@ sys.path.insert(1, "../../")
 from Utilities import loadDf,scaleData, PCAData
 from plot_set import *
 import matplotlib.pyplot as plt
+from matplotlib.legend_handler import HandlerTuple
 from scipy.spatial import ConvexHull
+from scipy import interpolate
 import numpy as np
 
 
@@ -17,19 +19,23 @@ def plotPCA():
     df = df.drop(columns=["wgt_SG", "channel"])
     df = scaleData(df)
     dfPCA = PCAData(df)
-    colors = ["khaki", "paleturquoise", "indigo", "pink", "chocolate"]
+    colors = ["khaki", "chocolate", "indigo", "pink", "chocolate"]
     alphas = np.linspace(1.0, .2, len(channelsU))
     skip = False
     
-    fig, (ax1, ax2) = plt.subplots(
-            1,
+    fig, axs = plt.subplots(
+            2,
             2,
             num=0,
             dpi=80,
             facecolor="w",
             edgecolor="k",
-            figsize=(7.4, 4.8),
+            figsize=(7.4, 7.4),
         )
+    ax1 = axs[0,0]
+    ax2 = axs[0,1]
+    ax3 = axs[1,0]
+    ax4 = axs[1,1]
     plotters = []
     legends = []
     for i in range(len(channelsU)):
@@ -50,46 +56,58 @@ def plotPCA():
             color = plt.rcParams["axes.prop_cycle"].by_key()["color"][i]
         except:
             color = colors[i-10]
+        print(color)
         m_1, m_2 = np.mean(dfPCA_0i), np.mean(dfPCA_1i)
         s_1, s_2 = np.std(dfPCA_0i), np.std(dfPCA_1i)
-        p_1 = dfPCA_0i[np.abs(dfPCA_0i - m_1) < s_1 ][:10]
-        p_2 = dfPCA_0i[np.abs(dfPCA_1i - m_2) < s_2 ][:10]
-        points = np.c_[p_1,p_2]
-        
+        p_1 = dfPCA_0i[np.abs(dfPCA_0i - m_1) < 0.5*s_1 ]
+        p_2 = dfPCA_0i[np.abs(dfPCA_1i - m_2) < 0.5*s_2 ]
+        points = np.c_[p_1[:10],p_2[:10]]
         s1= ax1.scatter(points[:,0], points[:,1], color = color)
 
-        hull = ConvexHull(points)
-        x_hull = np.append(points[hull.vertices,0],
-                        points[hull.vertices,0][0])
-        y_hull = np.append(points[hull.vertices,1],
-                        points[hull.vertices,1][0])
+        interp_x, interp_y = getCurve(points)
+        f2, = ax2.fill(interp_x, interp_y, '--', c=color, alpha=alphas[i], label = channel)
+        points = np.c_[p_1[:100],p_2[:100]]
+        interp_x, interp_y = getCurve(points)
+        ax3.fill(interp_x, interp_y, '--', c=color, alpha=alphas[i])
+        points = np.c_[p_1[:400],p_2[:400]]
+        interp_x, interp_y = getCurve(points)
+        ax4.fill(interp_x, interp_y, '--', c=color, alpha=alphas[i])
         
-        # interpolate
-        from scipy import interpolate
-        dist = np.sqrt((x_hull[:-1] - x_hull[1:])**2 + (y_hull[:-1] - y_hull[1:])**2)
-        dist_along = np.concatenate(([0], dist.cumsum()))
-        spline, u = interpolate.splprep([x_hull, y_hull], 
-                                        u=dist_along, s=0, per=1)
-        interp_d = np.linspace(dist_along[0], dist_along[-1], 50)
-        interp_x, interp_y = interpolate.splev(interp_d, spline)
-        # plot shape
-
-        f2 = ax2.fill(interp_x, interp_y, '--', c=color, alpha=alphas[i], label = channel)
-        ax2.set_yticks([])
-        ax1.grid(False)
-        ax2.grid(False)
+        
         plotters.append((s1,f2))
-        plotters.append(channel)
+        legends.append(channel)
 
-    plt.legend()
+    ax2.set_yticks([])
+    ax4.set_yticks([])
+    ax1.set_xticks([])
+    ax2.set_xticks([])
+    ax1.grid(False)
+    ax2.grid(False)
+    ax3.grid(False)
+    ax4.grid(False)
+    ax2.legend(plotters, legends, handler_map={tuple: HandlerTuple(ndivide=None)})
     plt.tight_layout()
-    plt.savefig("PCAPlot.pdf")
+    plt.savefig("PCAPlotFirst.pdf")
     plt.show()
 
 
 
 
-    
+def getCurve(points):
+    hull = ConvexHull(points)
+    x_hull = np.append(points[hull.vertices,0],
+                    points[hull.vertices,0][0])
+    y_hull = np.append(points[hull.vertices,1],
+                    points[hull.vertices,1][0])
+
+    # interpolate
+    dist = np.sqrt((x_hull[:-1] - x_hull[1:])**2 + (y_hull[:-1] - y_hull[1:])**2)
+    dist_along = np.concatenate(([0], dist.cumsum()))
+    spline, u = interpolate.splprep([x_hull, y_hull], 
+                                    u=dist_along, s=0, per=1)
+    interp_d = np.linspace(dist_along[0], dist_along[-1], 50)
+    interp_x, interp_y = interpolate.splev(interp_d, spline)
+    return interp_x, interp_y
 
 
 
