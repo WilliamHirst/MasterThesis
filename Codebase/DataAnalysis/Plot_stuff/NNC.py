f88d2ab5f8ebb380bc9eea8beb6e632630d3d086
William Oskar Holthe Hirst <wohirst@hepp03.hpc.uio.no>
2023-01-25 16:59:20 +0100

    Finished 3d comp.

--- Codebase/DataAnalysis/Plot_stuff/NNC.py
+++ Codebase/DataAnalysis/Plot_stuff/NNC.py
@@ -5,6 +5,7 @@ import sys
 sys.path.insert(1, "../../")
 sys.path.insert(1, "../")
 from Utilities import *
+from plot_set import *
 
 import numpy as np
 
@@ -21,7 +22,7 @@ def plotComp(metric):
 
     fig, ax = plt.subplots(figsize=(10,8))
 
-    methods = json_object.keys()
+    methods = list(json_object.keys())
 
     m1 = []
     m2 = []
@@ -42,25 +43,17 @@ def plotComp(metric):
         scores = []
         m1_i = method_0[i]["m1"]
         m2_i = method_0[i]["m2"]
-        
+
         for method in methods:
             score = json_object[method][f"{m1_i}_{m2_i}"]["score"]
             scores.append(score)
             # print(score)
         scores = 100*np.asarray(scores)/np.max(scores)
         scores = np.around(scores)
-        print(scores)
 
-        # Scores.append(scores_i)
-        # M1.append(int(m1_i))
-        # M2.append(int(m2_i))
+        draw_pie(scores, int(m1_i), int(m2_i), 1500, ax=ax)
 
-        # ax.scatter(x=int(m1_i), y=int(m2_i), s=0)
-        # ax.pie(np.array(Scores), center = (M1, M2), radius = 1)
-        print(m1, m2)
-        print(scores)
-        draw_pie(scores, m1_i, m2_i, 1500, ax=ax)
-
+    ax.legend(borderpad = 1,fontsize = 'x-large',loc =  'lower left', labels = methods,labelcolor = [color_cycle(2),color_cycle(3),color_cycle(4)],)
     fig.savefig("test.pdf")
 
 
@@ -79,14 +72,17 @@ def draw_pie(dist,
     cumsum = np.cumsum(dist)
     cumsum = cumsum/ cumsum[-1]
     pie = [0] + cumsum.tolist()
-
+    i = 2
     for r1, r2 in zip(pie[:-1], pie[1:]):
         angles = np.linspace(2 * np.pi * r1, 2 * np.pi * r2)
         x = [0] + np.cos(angles).tolist()
         y = [0] + np.sin(angles).tolist()
 
         xy = np.column_stack([x, y])
-        ax.scatter([xpos], [ypos], marker=xy, s=size)
+        ax.scatter([xpos], [ypos], marker=xy, s=size, color = color_cycle(i))
+        i += 1
+        
+    ax.scatter([xpos], [ypos], s=size, facecolor = 'none', edgecolors=color_cycle(2+np.where(dist ==100)[0][0]), linewidth = 2.5)
     return ax
 
 if __name__ == "__main__":
