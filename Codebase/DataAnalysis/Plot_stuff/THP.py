0ddc18d1a5d5ecffa850a01656d163f42ab31011
William Oskar Holthe Hirst <wohirst@hepp03.hpc.uio.no>
2023-02-10 17:14:44 +0100

    Added shallow NN plot

--- Codebase/DataAnalysis/Plot_stuff/THP.py
+++ Codebase/DataAnalysis/Plot_stuff/THP.py
@@ -16,5 +16,6 @@ def THP(history, model, signal):
     plt.plot(training, label = "Training")
     plt.plot(validation, label = "Validation")
     plt.legend(fontsize = 16)
+    plt.title(f"Model: {model}", fontsize = 16)
     plt.tight_layout(pad=1.1, w_pad=0.7, h_pad=0.2)
     plt.savefig(f"../../../thesis/Figures/MLResults/NN/{signal}/History/{model}History.pdf", bbox_inches="tight")
